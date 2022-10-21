0ab39e42c09a00d14ccc49384b9b43dd9c88197a
Surya Shakti <suryashakti1999@gmail.com>
2022-10-21 18:24:37 +0530

    Add new fields and new page showing status (#8)

--- ui/src/components/Form/index.js
+++ ui/src/components/Form/index.js
@@ -7,7 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useState } from "react";
-import { createDockerDesktopClient } from '@docker/extension-api-client';
+import { createDockerDesktopClient } from "@docker/extension-api-client";
 
 const Form = () => {
   const [username, setUsername] = useState("parseable");
@@ -21,6 +21,11 @@ const Form = () => {
   const [region, setRegion] = useState("us-east-1");
   const [localStorage, setLocalStorage] = useState("./data");
   const ddClient = createDockerDesktopClient();
+  const [deployed, setDeployed] = useState(false);
+  const [response, setResponse] = useState({});
+  const [ContainerId, setContainerId] = useState("");
+  const [portNumber, setPortNumber] = useState(8000);
+  const [volumePath, setVolumePath] = useState("/temp/data");
 
   const resetHandler = () => {
     setUsername("parseable");
@@ -34,123 +39,224 @@ const Form = () => {
   };
 
   async function runDockerParseable() {
-    const result = await ddClient.docker.cli.exec('run parseable/parseable:v0.0.5 parseable server --username ' +username+ ' --password ' +password+ ' --s3-endpoint-url ' + bucketUrl+' --s3-access-key-id ' +accessKey+ ' --s3-secret-key ' +secretKey+ ' --s3-region ' +region+ ' --s3-bucket-name ' +bucketName, []);
+    const result = ddClient.docker.cli.exec(
+      "run",
+      [
+        "-p",
+        `${portNumber}:8000`,
+        "-v",
+        `${volumePath}:/data`,
+        "parseable/parseable:v0.0.5",
+        "parseable",
+        "server",
+        "--username",
+        username,
+        "--password",
+        password,
+        "--s3-endpoint-url",
+        bucketUrl,
+        "--s3-access-key-id",
+        accessKey,
+        "--s3-secret-key",
+        secretKey,
+        "--s3-region",
+        region,
+        "--s3-bucket-name",
+        bucketName,
+      ],
+      {
+        stream: {
+          async onOutput() {
+            const containers = await ddClient.docker.listContainers();
+            console.log(
+              containers.length > 0 &&
+                containers.filter(
+                  (container) => container.Ports[0]?.PublicPort === 8000
+                )[0].Id
+            );
+            setContainerId(
+              containers.length > 0 &&
+                containers.filter(
+                  (container) => container.Ports[0]?.PublicPort === 8000
+                )[0].Id
+            );
+            setDeployed(true);
+          },
+          onClose(exitCode) {
+            console.log("onClose with exit code " + exitCode);
+          },
+          splitOutputLines: true,
+        },
+      }
+    );
   }
 
   return (
     <Container maxWidth="lg">
-      <Box>
-        {/* <Typography mb={2} textAlign={'center'} mt={5} variant="h6" color={"#666666"}>
-          Its optional to fill and parseable will start with default config if
-          fields are not filled.
-        </Typography> */}
-        <Typography fontWeight="bold" variant="h6" color={"#666666"}>
-          Login Credentials
-        </Typography>
-        <Box display="flex" width="100%" mt={2}>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              Username to Set for Parseable
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+      {deployed ? (
+        <Container maxWidth="lg">
+          <Box>
+            Congratulations you have successfully deployed parseable container
+            with ContainerId :{" "}
+            <span style={{ color: "green" }}>{ContainerId}</span> and running on{" "}
+            <span>
+              <a href="http://localhost:8000">http://localhost:8000</a>
+            </span>
           </Box>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              Password to Set for Parseable
+        </Container>
+      ) : (
+        <Container maxWidth="lg">
+          <Box>
+            <Typography fontWeight="bold" variant="h6" color={"#666666"}>
+              Login Credentials
             </Typography>
-            <TextField
-              id="outlined-basic"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Box display="flex" width="100%" mt={2}>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  Username to Set for Parseable
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={username}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Box>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  Password to Set for Parseable
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={password}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Box>
+            </Box>
           </Box>
-        </Box>
-      </Box>
-      <Box>
-        <Typography mt={2} fontWeight="bold" variant="h6" color={"#666666"}>
-          Environment Variables
-        </Typography>
-        <Box display="flex" width="100%" mt={2}>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              S3 / Compatible URL
+          <Box>
+            <Typography mt={2} fontWeight="bold" variant="h6" color={"#666666"}>
+              Docker Config
             </Typography>
-            <TextField
-              id="outlined-basic"
-              value={bucketUrl}
-              onChange={(e) => setBucketUrl(e.target.value)}
-            />
+            <Box display="flex" width="100%" mt={2}>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  Local Port Number
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={portNumber}
+                  type="number"
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setPortNumber(e.target.value)}
+                />
+              </Box>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  Local Volume Path
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={volumePath}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setVolumePath(e.target.value)}
+                />
+              </Box>
+            </Box>
           </Box>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              S3 / Compatible Bucket Name
+          <Box>
+            <Typography mt={2} fontWeight="bold" variant="h6" color={"#666666"}>
+              Environment Variables
             </Typography>
-            <TextField
-              id="outlined-basic"
-              value={bucketName}
-              onChange={(e) => setBucketName(e.target.value)}
-            />
+            <Box display="flex" width="100%" mt={2}>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  S3 / Compatible URL
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={bucketUrl}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setBucketUrl(e.target.value)}
+                />
+              </Box>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  S3 / Compatible Bucket Name
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={bucketName}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setBucketName(e.target.value)}
+                />
+              </Box>
+            </Box>
+            <Box display="flex" width="100%" mt={2}>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  S3 / Compatible Access Key
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={accessKey}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setAccessKey(e.target.value)}
+                />
+              </Box>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  S3 / Compatible Secret Key
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={secretKey}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setSecretKey(e.target.value)}
+                />
+              </Box>
+            </Box>
+            <Box display="flex" width="100%" mt={2}>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  S3 / Compatible Region
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={region}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setRegion(e.target.value)}
+                />
+              </Box>
+              <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
+                <Typography mb={1} variant="subtitle" color={"#666666"}>
+                  Local Data Storage Path
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  value={localStorage}
+                  sx={{ input: { color: "#A6A6A6" } }}
+                  onChange={(e) => setLocalStorage(e.target.value)}
+                />
+              </Box>
+            </Box>
+            <Box display="flex" width="100%" justifyContent="flex-end" mt={5}>
+              <Stack direction="row" spacing={4}>
+                <Button onClick={() => resetHandler()} variant="outlined">
+                  Reset
+                </Button>
+                <Button
+                  onClick={() => runDockerParseable()}
+                  variant="contained"
+                >
+                  Deploy
+                </Button>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-        <Box display="flex" width="100%" mt={2}>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              S3 / Compatible Access Key
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              value={accessKey}
-              onChange={(e) => setAccessKey(e.target.value)}
-            />
-          </Box>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              S3 / Compatible Secret Key
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              value={secretKey}
-              onChange={(e) => setSecretKey(e.target.value)}
-            />
-          </Box>
-        </Box>
-        <Box display="flex" width="100%" mt={2}>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              S3 / Compatible Region
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              value={region}
-              onChange={(e) => setRegion(e.target.value)}
-            />
-          </Box>
-          <Box display="flex" width="100%" flexDirection={"column"} mx={2}>
-            <Typography mb={1} variant="subtitle" color={"#666666"}>
-              Local Data Storage Path
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              value={localStorage}
-              onChange={(e) => setLocalStorage(e.target.value)}
-            />
-          </Box>
-        </Box>
-        <Box display="flex" width="100%" justifyContent="flex-end" mt={5}>
-          <Stack direction="row" spacing={4}>
-            <Button onClick={() => resetHandler()} variant="outlined">
-              Reset
-            </Button>
-            <Button onClick={() => runDockerParseable()} variant="contained">
-              Deploy
-            </Button>
-          </Stack>
-        </Box>
-      </Box>
+        </Container>
+      )}
     </Container>
   );
 };
--- ui/src/components/HeroSection/index.js
+++ ui/src/components/HeroSection/index.js
@@ -5,31 +5,39 @@ function HeroSection() {
   return (
     <Box>
       <Container maxWidth="lg">
-          <Box flex={11 / 12} item xs={8}>
+        <Box flex={11 / 12} item xs={8}>
+          <Box>
             <Box>
-              <Box>
-                <img
-                  width="250px"
-                  src={"https://www.parseable.io/img/Logo%20parseable.svg"}
-                  alt={""}
-                />
-              </Box>
-              <Typography mt={2} variant="body1" color={"#666666"}>
-                Parseable Docker extension is a simple, one-click solution to
-                deploy Parseable on your machine and start ingesting and analyzing
-                application and event logs.
-              </Typography>
-              <Typography mt={2} variant="h6" color={"#666666"}>
-                How to use this extension.
-              </Typography>
-              <Typography mt={1} variant="body1" color={"#666666"}>
-                Fill in the below fields with the relevant information, and click
-                on the "deploy" button. This will start Parseable on port 8000.
-                You can now navigate to http://localhost:8000 in your browser.
-                Refer the Parseable documentation for details: https://www.parseable.io/docs/deployment/docker 
-              </Typography>
+              <img
+                width="250px"
+                src={"https://www.parseable.io/img/Logo%20parseable.svg"}
+                alt={""}
+              />
             </Box>
+            <Typography mt={2} variant="body1" color={"#666666"}>
+              Parseable Docker extension is a simple, one-click solution to
+              deploy Parseable on your machine and start ingesting and analyzing
+              application and event logs.
+            </Typography>
+            <Typography mt={2} variant="h6" color={"#666666"}>
+              How to use this extension.
+            </Typography>
+            <Typography mt={1} variant="body1" color={"#666666"}>
+              Fill in the below fields with the relevant information, and click
+              on the "deploy" button. This will start Parseable on port 8000.
+              You can now navigate to{" "}
+              <span>
+                <a href={"http://localhost:8000"}>http://localhost:8000</a>
+              </span>{" "}
+              in your browser. Refer the Parseable documentation for details:
+              <span>
+                <a href="https://www.parseable.io/docs/deployment/docker">
+                  https://www.parseable.io/docs/deployment/docker
+                </a>
+              </span>
+            </Typography>
           </Box>
+        </Box>
       </Container>
       <br />
       <hr />
